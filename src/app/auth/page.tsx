0854692a5327118c088aea2fd2aef7f2d91130f3
Rui Lu <122314440+laughtale678@users.noreply.github.com>
2024-02-11 13:52:57 -0800

    added dark and light mode theme

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,17 @@
+
+
+function Auth() {
+  return (
+    <section className="container mx-auto">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
+          <div className="flex mb-8">
+
+          </div>
+
+
+        </div>
+    </section>
+  );
+}
+
+export default Auth;
